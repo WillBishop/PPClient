be0b0825b5f8c342c3dc67562b9f1cf46126d89b
Will Bishop <WillBishop@users.noreply.github.com>
2017-03-16 19:36:59 +1030

    Added Teacher names, class times, and presence
    Now in the class view, instead of just showing the class and note, it now shows a lot more information on the cell.

--- classListController.swift
+++ classListController.swift
@@ -5,13 +5,14 @@ import Alamofire
 import SwiftyJSON
 import KeychainSwift
 
-class diaryController: UIViewController, UITableViewDataSource, UITableViewDelegate { //Making the class conform to the UITableView's requirements
+class diaryController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 	
 	
 	@IBOutlet var diaryTable: UITableView!
 	
-	var classList = UserDefaults.standard.object(forKey: "cachedClasses") as? [String] ?? [String]() //Checks if the dayplan has been cached
-	var classNote = UserDefaults.standard.object(forKey: "cachedNotes") as? [String: String?] ?? [String: String!]() //"" ""
+	var classList = UserDefaults.standard.object(forKey: "cachedClasses") as? [String] ?? [String]()
+	var classNote = UserDefaults.standard.object(forKey: "cachedNotes") as? [String: String?] ?? [String: String!]()
+	var classInfo = UserDefaults.standard.object(forKey: "cachedInfo") as? [String: [String: Any]] ?? [String: [String: Any]]()
 	
 	
 	override func viewDidLoad() {
@@ -28,8 +29,6 @@ class diaryController: UIViewController, UITableViewDataSource, UITableViewDeleg
 		var sounds = ["1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th", "11th", "12th", "14th", "15th", "16th", "17th", "18th", "19th", "20th", "21st", "22nd", "23rd", "24th", "25th", "26th", "27th", "28th", "29th", "30th", "30st"]
 		var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 		self.navigationController?.navigationBar.topItem?.title = "Diary - \(sounds[day - 1]) of \(months[month - 1])"
-		//Above 9 lines is a hacky way of getting the date along with the sound of the number, couldn't find alternative, so for now this will do
-		
 		
 		diaryTable.delegate = self
 		diaryTable.dataSource = self
@@ -44,28 +43,36 @@ class diaryController: UIViewController, UITableViewDataSource, UITableViewDeleg
 		
 		Alamofire.request("https://da.gihs.sa.edu.au/stirling/classes/daily/get?username=\(username)&password=\(password)", method: .get)
 			.responseJSON { response in
-
 				if response.result.value != nil{
 					self.classList.removeAll()
 					self.classNote.removeAll()
 					let json = JSON(response.result.value!)
+					
 					for i in json["daymapDiaryClasses"]{
 						let name = String(describing: i.1["className"])
 						
 						self.classList.append(String(describing: i.1["className"]))
 						
 						if i.1["classNotes"].exists(){
-							print(i.1["classNotes"])
 							self.classNote[name] = String(describing: i.1["classNotes"])
 						} else {
 							self.classNote[name] = "No lesson plans have been entered for this lesson"
 						}
+						var attendance = "✓"
+						if String(describing: i.1["classAttendance"]) == "PRESENT"{
+							let attendance = "✓"
+						} else {
+							let attendance = "✘"
+						}
+						self.classInfo[name] = ["time": "\(String(describing: i.1["startTime"])) until \(String(describing: i.1["endTime"]))", "room": String(describing: i.1["roomName"]), "teacher":   String(describing: (i.1["teacherName"])), "presence": attendance]
 					}
 					
 					UserDefaults.standard.set(self.classList, forKey: "cachedClasses")
 					UserDefaults.standard.set(self.classNote, forKey: "cachedNotes")
+					UserDefaults.standard.set(self.classInfo, forKey: "cachedInfo")
 					
-					print(self.classList)
+					
+					print(self.classInfo)
 					
 				} else {
 					//TODO Better error managment, this is just here to prevent an app crash, while still retaining some useful information for debugging
@@ -85,17 +92,23 @@ class diaryController: UIViewController, UITableViewDataSource, UITableViewDeleg
 	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = diaryTable.dequeueReusableCell(withIdentifier: "class", for: indexPath) as! diaryList
 		cell.name = classList[indexPath.row]
-		var className = classList[indexPath.row]
-		
+		let className = classList[indexPath.row]
 		if classNote[classList[indexPath.row]] != nil{
 			cell.note = (classNote[className])!!
+
+
 		}
+		if classInfo.count > 0{
+			cell.teacher = classInfo[className]?["teacher"] as! String
+			cell.time = classInfo[className]?["time"] as! String
+			cell.presence = "Presence: \(classInfo[className]?["presence"] as! String)"
+		}
+		
 		cell.update()
 		
 		return cell
 		
 	}
-	
 	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 		return classList.count
 	}
@@ -116,15 +129,26 @@ class diaryController: UIViewController, UITableViewDataSource, UITableViewDeleg
 	
 	
 }
-class diaryList : UITableViewCell{ //Custom cell class, as I couldn't figure out how to have multiple labels in the one cell without it.
+class diaryList : UITableViewCell{
 	var name = ""
 	var note = ""
+	var teacher = ""
+	var time = ""
+	var presence = ""
 	
 	@IBOutlet weak var className: UILabel!
 	@IBOutlet weak var classNote: UILabel!
+	@IBOutlet weak var classPresence: UILabel!
+	@IBOutlet weak var classTime: UILabel!
+	@IBOutlet weak var classInfo: UILabel!
 	
+	
+	//TODO: Add other class info, such as teachers, presence, time etc
 	func update() {
 		className.text = name
 		classNote.text = note
+		classPresence.text = presence
+		classTime.text = time
+		classInfo.text = teacher
 	}
 }
