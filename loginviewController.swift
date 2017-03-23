82a25afc88377e28c55cc9c4b7a69b7c4c916dc7
Will Bishop <WillBishop@users.noreply.github.com>
2017-03-23 21:51:43 +1030

    New UI, Using CuRRiC/CURRIC/curric doesn't matter

--- loginviewController.swift
+++ loginviewController.swift
@@ -20,12 +20,14 @@ class loginviewController: UIViewController {
 	@IBOutlet weak var daymapPassword: UITextField!
 
 	
-	@IBOutlet weak var loginProcess: UIActivityIndicatorView!
+//	@IBOutlet weak var loginProcess: UIActivityIndicatorView!
 	
 	@IBOutlet weak var loginButton: UIButton!
 	
 	//@IBOutlet weak var signupButton: UIButton!
     override func viewDidLoad() {
+		daymapUsername.attributedPlaceholder = NSAttributedString(string: "Stirling Username", attributes: [NSForegroundColorAttributeName: UIColor.white])
+		daymapPassword.attributedPlaceholder = NSAttributedString(string: "Stirling Password", attributes: [NSForegroundColorAttributeName: UIColor.white])
         super.viewDidLoad()
 		loginButton.backgroundColor = UIColor(red: 0.20, green: 0.59, blue: 0.86, alpha: 1)
 		daymapUsername.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.2)
@@ -46,22 +48,26 @@ class loginviewController: UIViewController {
     }
 	
 	func checkCredentials(){
+		let username = daymapUsername.text!.lowercased().replacingOccurrences(of: "curric\\", with: "")
+		print(username)
+		let password = daymapPassword.text!
+		
 		print("Checking")
 		Alamofire.request("https://daymap.gihs.sa.edu.au/daymap/student/dayplan.aspx", method: .get)
-			.authenticate(user: daymapUsername.text!, password: daymapPassword.text!) //Alamofire does auto NTLM
+			.authenticate(user: username, password: password) //Alamofire does auto NTLM
 			.responseString { response in
 				print("Got response code \(String(describing: response.response?.statusCode))")
 				if response.response?.statusCode == 200{
 					self.credentialsCorrect()
-					self.loginProcess.stopAnimating()
+					//self.loginProcess.stopAnimating()
 				}
 				else if response.response?.statusCode == 401{
 					self.cannotfindSchool.text = "Incorrect Username or Password"
-					self.loginProcess.stopAnimating()
+					//self.loginProcess.stopAnimating()
 				}
 				else if response.response?.statusCode != 200 && response.response?.statusCode != 401{
 					self.cannotfindSchool.text = "Error, please try again later."
-					self.loginProcess.stopAnimating()
+					//self.loginProcess.stopAnimating()
 				}
 				
 		}}
@@ -78,16 +84,15 @@ class loginviewController: UIViewController {
 	func credentialsCorrect(){//In future I will add login validation, this is the groundwork.
 		let keychain = KeychainSwift()
 
-		keychain.set(daymapUsername.text!, forKey: "username")
+		keychain.set(daymapUsername.text!.lowercased().replacingOccurrences(of: "curric\\", with: ""), forKey: "username")
 		keychain.set(daymapPassword.text!, forKey: "password")
 		UserDefaults.standard.set("yes", forKey: "isloggedIn")
 		loggedIn()}
 	
 	@IBAction func submitCreds(_ sender: Any) {
-		print("Initiating Login")
-		loginProcess.startAnimating()
 		checkCredentials()
 	}
+	
 	
 
 }
